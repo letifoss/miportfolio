4e663815a326a20707b48e1fd43c6525a2f9ac21
letifoss <letifossemale@gmail.com>
2024-04-29 21:04:22 -0300

    presentacion animacion

--- src/componentes/Presentacion.js
+++ src/componentes/Presentacion.js
@@ -6,10 +6,10 @@ function Presentacion(){
     <div id="presentacion">
         <div className="content d-flex justify-content-center align-items-center">
             <img src={fotolinkedin} alt="foto leticia fossemale" width="260"/>
-            <div className="d-flex flex-column align-items-center text-right">
+            <div className="d-flex flex-column">
                 <h1 className="text-center">Leticia Fossemale</h1>
-                <h5 className="">Bienvenidos a mi portfolio. Aquí conocerás un poco más sobre mí, mis estudios y te mostraré algunos de mis proyectos. </h5>
-                <h5> Soy <b><i>desarrolladora full stack</i></b>, aunque mi enfoque está en el desarrollo frontend.</h5>
+                <h4 className="text-rigth">Bienvenidos a mi portfolio. Aquí conocerás un poco más sobre mí, mis estudios y te mostraré algunos de mis proyectos. </h4>
+                <h4 className="text-rigth">Soy de Uruguay, tengo 28 años y soy <b><i>desarrolladora full stack</i></b>. Actualmente me estoy enfocando más en el desarrollo frontend, especificamente en React.</h4>
             </div>
         </div>
     </div>
